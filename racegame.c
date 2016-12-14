d2f19cac691d2e60be039adf1ff0e7c1e6f3f355
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-12-14 00:11:25 -0500

    Update racegame.c

--- racegame.c
+++ racegame.c
@@ -69,9 +69,9 @@ while (1 && car[0]<40) {
 }
 
 void* ai() {
-for (i=1; i<4; i++) {
+for (int i=1; i<4; i++) {
 	while (1 && car[i]<40) {
-		usleep(rand() %0.1 + 0); //0-100 milliseconds
+		usleep(rand() %0.1 + 0.0); //0-100 milliseconds
 		car[i]++;
 	}
 }
